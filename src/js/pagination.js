df25ed3dcf181d01e890cec0412428b86248219f
Sutan Nst <sgnzst@gmail.com>
2018-11-03 02:03:14 +0700

    added animated loading gif

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -4,21 +4,36 @@ function paginateBlog(that, event) {
   $.ajax({
     method: 'GET',
     url: pageUrl,
-    dataType: 'HTML'
+    dataType: 'HTML',
+    beforeSend: function() {
+      swal({
+        icon: '/assets/img/tools/loading.gif',
+        text: "Sedang Proses...",
+        buttons: false,
+        closeOnClickOutside: false,
+        closeOnEsc: false
+      });
+    }
   }).done(function(response) {
     var
       htmlData = $(response).find('#content').html(),
       title = $(response).filter('title').text();
+    swal.close();
     $('#content').html(htmlData);
     document.title = title;
     window.history.pushState({html: htmlData, title: title}, title, pageUrl);
     window.onpopstate = function(event){
       if(event.state){
-        $('#scroll').html(event.state.html);
+        $('#content').html(event.state.html);
         document.title = event.state.title;
       }
     };
   }).fail(function(response, stats, xhr) {
-    swal({icon: "error", title: "Error", text: response.status+" "+xhr});
+    swal.close();
+    swal({
+      icon: "error",
+      title: "Error",
+      text: response.status+" "+xhr
+    });
   });
 }
