e3216efe96d5fb6cbc1faef5c6aeb5f959485d8e
Sutan Nst <sutan.gnst@gmail.com>
2019-01-20 23:50:38 +0700

    modified Global.js

--- src/js/Global.js
+++ src/js/Global.js
@@ -1,15 +1,15 @@
 function documentLoad(e, that){
   e.preventDefault()
   let link = $(this).attr('href') ? $(this).attr('href') : $(that).attr('href')
+  $("#sidebar,a#slide,#fade").removeClass("slide")
+  $("#open").show()
+  $("#search").show()
+  $("#close").hide()
   swal({
     icon: '/assets/img/tools/loading.gif',
     text: "Sedang Proses...",
     buttons: false,
     closeOnEsc: false,
-    closeOnClickOutside: false
   })
-  setTimeout(function(){
-    swal.close()
-    window.location.href = link
-  }, 750)
+  setTimeout(() => { window.location.href = link }, 750)
 }
