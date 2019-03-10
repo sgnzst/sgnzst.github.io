65dd080200a714a39118185587825d21d9f3f084
Sutan Nst <sutan.gnst@gmail.com>
2019-03-10 17:45:04 +0700

    remove posts

--- contents/index.js
+++ contents/index.js
@@ -1,5 +1,4 @@
 export default [
-  { name: 'apakah-php-akan-mati', dateMonth: '2018/10' },
   { name: 'berkenalan-dengan-pdo-di-php', dateMonth: '2018/10' },
   { name: 'it-fest-bersama-it-polsri', dateMonth: '2018/11' }
 ]
