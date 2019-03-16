b54b9e84481ab202a09528703089739da77c179b
Sutan Nst <sutan.gnst@gmail.com>
2019-03-16 12:15:13 +0700

    add test

--- test/Default.spec.js
+++ test/Default.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import Default from '@/layouts/default'
 
 describe('Logo', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
+    const wrapper = mount(Default)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
