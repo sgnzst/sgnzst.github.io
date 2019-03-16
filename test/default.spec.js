64d44a20fd1348c03924cf7a9271a4ae49759c02
Sutan Nst <sutan.gnst@gmail.com>
2019-03-16 12:13:08 +0700

    Revert "modified add default.spec test"
    This reverts commit 7eebcc8db24e21c1ff1c53c6c6e30068365213ef.

--- test/default.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Default from '@/layouts/default'
-
-describe('Default', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(Default)
-    expect(wrapper.isVueInstance()).toBeTruthy()
-  })
-})
