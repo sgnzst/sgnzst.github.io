aac79526b81dfd743c12616e333199e2f485bf93
Sutan Nst <sutan.gnst@gmail.com>
2019-03-16 13:10:07 +0700

    remove exporess server

--- components/Base/Banner.vue
+++ components/Base/Banner.vue
@@ -44,7 +44,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #banner-figure {
   position: absolute;
   display: flex;
--- components/Homepage/About.vue
+++ components/Homepage/About.vue
@@ -88,7 +88,7 @@ export default {
 }
 </script>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
 p {
   font-size: 18px;
   color: #555;
