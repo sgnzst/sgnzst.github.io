3c60718b5f0f6b9bdd3d3473005dd1495d714f6b
Sutan Nst <sutan.gnst@gmail.com>
2019-04-16 22:42:18 +0700

    committing site Sel Apr 16 22:42:18 WIB 2019

--- components/Homepage/About.vue
+++ components/Homepage/About.vue
@@ -13,7 +13,7 @@
         data-theme="light"
         data-vanity="sutan-gading-fadhillah-nasution-963841160"
       >
-        <a target="_blank" href="https://id.linkedin.com/in/sutan-gading-fadhillah-nasution-963841160?trk=profile-badge">
+        <a rel="noopener" target="_blank" href="https://id.linkedin.com/in/sutan-gading-fadhillah-nasution-963841160?trk=profile-badge">
           <v-hover>
             <v-avatar slot-scope="{ hover }" :class="`my-2 elevation-${hover ? 12 : 4}`" size="200px">
               <img title="Sutan Nst." src="/assets/img/collections/sutan_formal.jpg" alt="Sutan Nst.">
