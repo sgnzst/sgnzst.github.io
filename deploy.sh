100768434e3c7b15ba4556c19507b32f50182fc3
Sutan Nst <sutan.gnst@gmail.com>
2019-05-04 08:55:54 +0700

    modified deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo -e "\n\033[0;32m> Start deploying updates to Github Pages...\033[0m"
-
 echo -e "\n\033[0;32m> Generating site...\033[0m"
 
 npm run generate
@@ -14,4 +12,4 @@ git commit -m "auto committing changes in `date`"
 
 npm run deploy
 
-echo -e "\n\033[0;32m> Site Deployed!\033[0m"
+echo -e "\n\033[0;32m> Site Deployed!\033[0m\n"
