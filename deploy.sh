78be4365b1874e36ccef21d6c075ee1d87dd67c0
Sutan Nst <sutan.gnst@gmail.com>
2019-05-04 08:48:43 +0700

    auto committing changes in Sab Mei  4 08:48:43 WIB 2019

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-echo -e "\033[0;32m> Start deploying updates to Github Pages...\033[0m"
+echo -e "\n\033[0;32m> Start deploying updates to Github Pages...\033[0m"
 
-echo -e "\033[0;32m> Generating site...\033[0m"
+echo -e "\n\033[0;32m> Generating site...\033[0m"
 
 npm run generate
+
+echo -e "\n"
 
 git add .
 
 git commit -m "auto committing changes in `date`"
 
-echo -e "\033[0;32m> Deploying..\033[0m"
+echo -e "\n\033[0;32m> Deploying..\033[0m"
 
 npm run deploy
 
-echo -e "\033[0;32m> Site Deployed!\033[0m"
+echo -e "\n\033[0;32m> Site Deployed!\033[0m"
