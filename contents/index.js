60180274d246f0cdfba78104e47c65844bfcc5df
Sutan Nst <sutan.gnst@gmail.com>
2019-05-27 18:38:35 +0700

    clean contents

--- contents/index.js
+++ contents/index.js
@@ -1,9 +1,7 @@
 /* eslint-disable */
 
 export default [
-  { name: 'berkenalan-dengan-pdo-di-php', date: '2018/10' },
   { name: 'it-fest-bersama-it-polsri', date: '2018/11' },
-  { name: 'iri', date: '2019/05' },
   { name: 'tak-akan-datang', date: '2019/05' },
 	{ name: 'masa-depan', date: '2019/05' }
 ]
